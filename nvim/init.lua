1de3d1d608c3ebac42c94f0b8fd488afdfe5e307
Ali Bencheikh <alibencheikh212@gmail.com>
2023-12-12 17:47:35 +0100

    Neovim configuration files and plugins

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+require("settings")
+require("plugins")
+require("maps")
+local themeStatus, kanagawa = pcall(require, "kanagawa")
+
+if themeStatus then
+  vim.cmd("colorscheme kanagawa")
+else
+  return
+end
