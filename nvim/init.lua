7127405eafe901ce7fc5e0dfb29ce2be831d5d6d
Ali Bencheikh <alibencheikh212@gmail.com>
2024-01-04 05:42:36 +0100

    update the init.lua and the readme file

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,19 +3,6 @@ require("plugins")
 require("maps")
 require("nvim-web-devicons").setup({ default = true })
 require("mason").setup()
-require("mason-lspconfig").setup()
-require("telescope").load_extension("fzf")
-require("telescope").setup({
-	extensions = {
-		fzf = {
-			fuzzy = true, -- false will only do exact matching
-			override_generic_sorter = true, -- override the generic sorter
-			override_file_sorter = true, -- override the file sorter
-			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-			-- the default case_mode is "smart_case"
-		},
-	},
-})
 -- setup for the icons
 require("nvim-web-devicons").setup({
 	default = true,
@@ -28,14 +15,6 @@ require("nvim-web-devicons").setup({
 	},
 })
 -- Theme
--- local themeStatus, kanagawa = pcall(require, "kanagawa")
-local themeStatus, tokyonight = pcall(require, "tokyonight")
--- local themeStatus, catppuccin = pcall(require, "catppuccin")
-
-if themeStatus then
-	-- vim.cmd("colorscheme kanagawa")
-	vim.cmd("colorscheme tokyonight-night")
-	-- vim.cmd("colorscheme catppuccin")
-else
-	return
-end
+-- uncomment the theme you want to use and comment the others
+require("configs.catppuccin")
+--require("configs.tokyonight")
