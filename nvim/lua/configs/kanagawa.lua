f666f420fb37c076bfa07138e11cf250729621c1
Ali Bencheikh <alibencheikh212@gmail.com>
2024-01-07 02:42:27 +0100

    update the kanagawa theme

--- nvim/lua/configs/kanagawa.lua
+++ nvim/lua/configs/kanagawa.lua
@@ -1,30 +1,28 @@
-local status, kanagawa = pcall(require, "kanagawa")
-
-if not status then
-	return
-end
-
-kanagawa.setup({
+-- Default options:
+require("kanagawa").setup({
+	compile = false, -- enable compiling the colorscheme
 	undercurl = true, -- enable undercurls
 	commentStyle = { italic = true },
 	functionStyle = {},
 	keywordStyle = { italic = true },
 	statementStyle = { bold = true },
 	typeStyle = {},
-	variablebuiltinStyle = { italic = true },
-	specialReturn = true, -- special highlight for the return keyword
-	specialException = true, -- special highlight for exception handling keywords
 	transparent = false, -- do not set background color
 	dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-	globalStatus = false, -- adjust window separators highlight for laststatus=3
 	terminalColors = true, -- define vim.g.terminal_color_{0,17}
-	colors = {},
-	overrides = {},
-	theme = "wave", -- Load "default" theme or the experimental "light" theme
+	colors = { -- add/modify theme and palette colors
+		palette = {},
+		theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+	},
+	overrides = function(colors) -- add/modify highlights
+		return {}
+	end,
+	theme = "wave", -- theme name : wave, lotus, dragon
 	background = {
 		dark = "wave",
 		light = "lotus",
 	},
 })
 
+-- setup must be called before loading
 vim.cmd.colorscheme("kanagawa")
