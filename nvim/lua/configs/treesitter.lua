2970f4097703e32c959edfa5952ee1cfbcf80d85
Ali Bencheikh <alibencheikh212@gmail.com>
2024-01-19 23:05:06 +0100

    new parser names in the ensure_installed config

--- nvim/lua/configs/treesitter.lua
+++ nvim/lua/configs/treesitter.lua
@@ -1,48 +1,52 @@
 local status, ts = pcall(require, "nvim-treesitter.configs")
 if not status then
-	return
+  return
 end
 
 ts.setup({
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
-	},
-	ensure_installed = {
-		"markdown",
-		"tsx",
-		"typescript",
-		"javascript",
-		"toml",
-		"c",
-		"go",
-		"bash",
-		"java",
-		"php",
-		"python",
-		"c_sharp",
-		"json",
-		"yaml",
-		"rust",
-		"css",
-		"html",
-		"lua",
-	},
-	sync_install = false,
-	auto_install = true,
-	rainbow = {
-		enable = true,
-		disable = { "html" },
-		extended_mode = false,
-		max_file_lines = nil,
-	},
-	autotag = { enable = true },
-	incremental_selection = { enable = true },
-	indent = { enable = true },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
+  ensure_installed = {
+    "bash",
+    "html",
+    "javascript",
+    "json",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "query",
+    "regex",
+    "tsx",
+    "typescript",
+    "vim",
+    "yaml",
+    "toml",
+    "c",
+    "go",
+    "java",
+    "php",
+    "c_sharp",
+    "rust",
+    "css",
+  },
+  sync_install = false,
+  auto_install = true,
+  rainbow = {
+    enable = true,
+    disable = { "html" },
+    extended_mode = false,
+    max_file_lines = nil,
+  },
+  autotag = { enable = true },
+  incremental_selection = { enable = true },
+  indent = { enable = true },
 })
 
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
